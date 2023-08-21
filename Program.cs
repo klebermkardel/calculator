189e48ae57c2f83898da76a3ec2f77fec42ecdb0
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-21 12:48:11 -0300

    :sparkles: feat: cria função de que faz a subtração dos valores

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtracao();
         }
 
         static void Soma() {
@@ -21,6 +21,20 @@ namespace Calculadora
 
             float resultado = v1 + v2;
             Console.WriteLine($"O resultado da soma é {resultado}");
+        }
+
+        static void Subtracao() {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+            
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtracao é {resultado}");
         }
     }
 }
