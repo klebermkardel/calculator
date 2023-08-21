9eb6764a55b303d4d7bd454dbfb7867a6a0d5dc3
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-21 12:26:14 -0300

    :sparkles: feat: Armazena valor informado pelo usuário em um variável 'v1' convertendo-o para número real e repete os mesmos passos com o segundo valor

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@ namespace Calculadora
         {
             Console.Clear();
             Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Segundo valor: ");
+            floar v2 = float.Parse(Console.ReadLine());
         }
     }
 }
